5980e61d5e1291fbca988ca34c1c3068a3326e1c
oscotten <oscotten@parktudor.org>
2017-08-31 12:10:47 -0400

    Game Class and Driver Creation

--- Game/src/Driver.java
+++ Game/src/Driver.java
@@ -2,6 +2,7 @@ public class Driver {
     public static void main(String[] args) {
 
         Game g = new Game();
+        g.START();
 
     }
 }
--- Game/src/Game.java
+++ Game/src/Game.java
@@ -7,9 +7,15 @@ public class Game {
 
     Dictionary dictionary;
 
-    ArrayList<String> DictionaryList;
+//    ArrayList<String> DictionaryList;
 
-    int wordLength;
+    int wordLength = 0;
+
+    int remainingGuesses = 0;
+
+    boolean runningTotal = false;
+
+    boolean play = true;
 
     public Game() {
 
@@ -17,23 +23,159 @@ public class Game {
 
         dictionary = new Dictionary();
 
-        DictionaryList = dictionary.getDictionaryList();
-
-        getUserLength();
+//        DictionaryList = dictionary.getDictionaryList();
 
     }
 
-    private void getUserLength(){
+    public void START(){
 
-        System.out.println("Input Word Length: ");
+        getUserLength();
+
+        getGuess();
+
+        runningTotal();
+
+        RUN();
+
+    }
+
+    private void getUserLength() {
+
+        boolean clear = true;
+
+        System.out.print("Input Word Length: ");
 
         try {
             wordLength = in.nextInt();
+        } catch (Exception e) {
+            inputError();
+            in.nextLine();
+            clear = false;
         }
-        catch (Exception e){
 
+        if (!clear) {
+            getUserLength();
         }
 
+        else if (dictionary.getWords(wordLength) == 0){
+            logicError();
+            getUserLength();
+        }
+
+    }
+
+    private void getGuess(){
+
+        boolean clear = true;
+
+        System.out.print("Input Number of Guesses: ");
+
+        try {
+            remainingGuesses = in.nextInt();
+        } catch (Exception e) {
+            inputError();
+            in.nextLine();
+            clear = false;
+        }
+
+        if (!clear) {
+            getGuess();
+        }
+
+        else if (remainingGuesses <= 0){
+            logicError();
+            getGuess();
+        }
+
+    }
+
+    private void runningTotal(){
+
+        boolean clear = true;
+
+        String check = "";
+
+        System.out.print("Number of Words Remaining (Y,N): ");
+
+        try {
+            check = in.next();
+            check = check.toUpperCase();
+        } catch (Exception e) {
+            inputError();
+            in.nextLine();
+            clear = false;
+        }
+
+        if (!clear) {
+            runningTotal();
+        }
+
+        else if (check.equals("Y") || check.equals("N")){
+            if (check.equals("Y")){
+                runningTotal = true;
+            }
+            else if (check.equals("N")){
+                runningTotal = false;
+            }
+            else{
+                logicError();
+                runningTotal();
+            }
+        }
+
+        else{
+            logicError();
+            runningTotal();
+        }
+
+    }
+
+    private String getCharacterGuess(){
+
+        boolean clear = true;
+
+        String character = "";
+
+        System.out.print("Input Character Guess: ");
+
+        try {
+            character = in.next();
+        } catch (Exception e) {
+            inputError();
+            in.nextLine();
+            clear = false;
+        }
+
+        if (!clear) {
+            getCharacterGuess();
+        }
+
+        else if (character.length() == 0 || character.length() > 1){
+            logicError();
+            getCharacterGuess();
+        }
+
+        return character;
+    }
+
+    private void RUN(){
+
+        WordFamily wf = new WordFamily(dictionary.getDictionaryList(),wordLength);
+
+        Character c = getCharacterGuess().charAt(0);
+
+        ArrayList<String> wfI = wf.identifyWords(c);
+
+        SpecificFamily sf = new SpecificFamily(wf,wfI,)
+
+    }
+
+    private void inputError(){
+        System.out.println("Input Error");
+    }
+
+    private void logicError(){
+        System.out.println("Logic Error");
     }
 
 }
